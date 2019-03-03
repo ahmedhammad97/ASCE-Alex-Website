5cdfc0b7475704c7c4b477cf5d53acd1bc4a7438
AhmedHammad <hammad97official@gmail.com>
2019-03-03 23:50:01 +0200

    Add NotFound Page

--- src/components/Pages/NotFoundPage.vue
+++ src/components/Pages/NotFoundPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="error">
-
+    <p><span class="oops">Oops!</span>
+      <br />
+      This page is not available, please visit a correct URL.
+      <br /><br />
+      <router-link to="/" id="back">Home Page</router-link>
+    </p>
   </div>
 </template>
 
@@ -14,5 +19,25 @@ export default {
 </script>
 
 <style scoped>
+p{
+  font-size: 30px;
+}
+.oops{
+  font-size: 50px;
+}
+#back{
+  background-color: #0c5fa8;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 30px;
+  border: 2px solid #0c5fa8;
+  transition: 0.3s ease-in-out;
+}
+#back:hover{
+  text-decoration: none;
+  background-color: transparent;
+  color: #0c5fa8;
 
+}
 </style>
