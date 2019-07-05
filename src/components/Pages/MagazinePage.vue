71429cb376d59185cc6f0d6fd4e9770299fa6229
AhmedHammad <hammad97official@gmail.com>
2019-07-05 14:09:26 +0200

    Add second edition of magazine

--- src/components/Pages/MagazinePage.vue
+++ src/components/Pages/MagazinePage.vue
@@ -4,13 +4,26 @@
     <p class="intro">At ASCE (American Society of Civil Engineers), we worked on providing the important scientific content through our annual mega event "the construction conference" and seminars. Yet, we found the significance of presenting the latest topics and technology in civil and architecture engineering in a form of magazine that is published every six months.<br />The magazine content covers the latest and modern and remarkable moves  in construction and architecture field. Thus, we can cope with our era's technology since we are the future leaders of engineering!<br /><br />ASCE AlexSC magazine first edition:<br /></p>
 
     <div class="issuuMag">
-      <h2>English version</h2>
+      <h2>Second Edition - English</h2>
+      <iframe src="https://e.issuu.com/anonymous-embed.html?u=ascealexsc1&d=english_version" style="border:none;width:100%;height:500px;" allowfullscreen></iframe>
+    </div>
+    <br />
+
+    <div class="issuuMag">
+      <h2>Second Edition - Arabic</h2>
+      <iframe src="https://e.issuu.com/anonymous-embed.html?u=ascealexsc1&d=arabic_version" style="border:none;width:100%;height:500px;" allowfullscreen></iframe>
+    </div>
+
+    <br /><br /><br />
+
+    <div class="issuuMag">
+      <h2>First Edition - English</h2>
       <iframe src="https://e.issuu.com/anonymous-embed.html?u=ascealexsc1&d=english" style="border:none;width:100%;height:500px;" allowfullscreen></iframe>
     </div>
     <br />
 
     <div class="issuuMag">
-      <h2>Arabic version</h2>
+      <h2>First Edition - Arabic</h2>
       <iframe src="https://e.issuu.com/anonymous-embed.html?u=ascealexsc1&d=arabic__2_" style="border:none;width:100%;height:500px;" allowfullscreen></iframe>
     </div>
 
