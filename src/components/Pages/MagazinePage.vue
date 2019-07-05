73b66a20189b3e6a8a36cafcb7a16523115eabbd
AhmedHammad <hammad97official@gmail.com>
2019-07-05 14:22:34 +0200

    Fix magazine page wrong title

--- src/components/Pages/MagazinePage.vue
+++ src/components/Pages/MagazinePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="magazine">
     <h1>ASCE Alex Digital Magazine</h1>
-    <p class="intro">At ASCE (American Society of Civil Engineers), we worked on providing the important scientific content through our annual mega event "the construction conference" and seminars. Yet, we found the significance of presenting the latest topics and technology in civil and architecture engineering in a form of magazine that is published every six months.<br />The magazine content covers the latest and modern and remarkable moves  in construction and architecture field. Thus, we can cope with our era's technology since we are the future leaders of engineering!<br /><br />ASCE AlexSC magazine first edition:<br /></p>
+    <p class="intro">At ASCE (American Society of Civil Engineers), we worked on providing the important scientific content through our annual mega event "the construction conference" and seminars. Yet, we found the significance of presenting the latest topics and technology in civil and architecture engineering in a form of magazine that is published every six months.<br />The magazine content covers the latest and modern and remarkable moves  in construction and architecture field. Thus, we can cope with our era's technology since we are the future leaders of engineering!<br /><br /></p>
 
     <div class="issuuMag">
       <h2>Second Edition - English</h2>
